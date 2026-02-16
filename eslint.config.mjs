bd9a56e614db40d026596106f216701a9f99f63b
Amadou <atidiane.camara@gmail.com>
2026-02-16 14:44:22 +0100

    feat: add users tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,9 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.jest },
+    },
     rules: {
       'no-var': 'error',
       'prefer-const': 'error',
